296a51102052897fe65c5456832b7da1a0631ff8
Daniel Gould <dmgould310@gmail.com>
2020-08-26 13:06:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,16 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+
   
 end
+
+
+
+
+
+
+
+
+
+  # Remember, you can access your database connection anywhere in this class
+  #  with DB[:conn]  
